f82c94fe5b176de7f3139821be070c169705a763
seb999 <sebastien0099@gmail.com>
2023-02-04 21:57:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BlockTime',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
